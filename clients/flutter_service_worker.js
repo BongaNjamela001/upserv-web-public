9eb052623714afd7c28f3aa1e6d988bb40e23b14
BongaNjamela001 <njmlun002@myuct.ac.za>
2026-04-12 17:58:15 +0200

    Fixed bug with Continue to Profile Setup button.

--- clients/flutter_service_worker.js
+++ clients/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "cb778c3902511274c7a7013d64d23724",
-"/": "cb778c3902511274c7a7013d64d23724",
-"flutter_bootstrap.js": "9cda517fe47d9edf81d318f7902fee92",
+const RESOURCES = {"index.html": "f7282a1e36d849ea772135a734fba9f1",
+"/": "f7282a1e36d849ea772135a734fba9f1",
+"flutter_bootstrap.js": "dd92599f6c98fc6dbdb7f8d420c836c1",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "assets/packages/pay_platform_interface/pubspec.yaml": "576a151a92ea9fc3da773ad9e9b4864b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
@@ -280,7 +280,7 @@ const RESOURCES = {"index.html": "cb778c3902511274c7a7013d64d23724",
 "assets/AssetManifest.bin": "c5bb4458dbb878784e126d493b84e6cc",
 "assets/AssetManifest.json": "880d33b4f7cb174a179a8cb675059556",
 "favicon.png": "1996ba4abd979e244338cc111fb2fd39",
-"main.dart.js": "371557be672242def1b0d7e27293fcb6",
+"main.dart.js": "1997e5b12488639029c618c0c0f4909e",
 "manifest.json": "be75d3332ba08eb207558280191b52e2",
 "canvaskit/skwasm.wasm": "264db41426307cfc7fa44b95a7772109",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
